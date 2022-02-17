dbb8d31067338735484eff0b2eb8140edddbf6c5
Brandon Austin <b.austin7007@gmail.com>
2022-02-16 16:33:39 -0800

    start conversation flow

--- pages/Conversation.js
+++ pages/Conversation.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components/native";
 import ChatBubble from "../components/ChatBubble";
 import { SafeAreaView } from "react-native";
 import Button from "../components/Button";
+const conversationData = require("../conversation.json");
 
 const Conversation = () => {
+  const [convo, setConvo] = useState([
+    conversationData.nodes[conversationData.startNodeId],
+  ]);
+  const [buttons, setButtons] = useState([]);
+
+  useEffect(() => {
+    let convoCopy = convo;
+    if (convo[0].childrenIds.length === 1) {
+      setConvo([...convoCopy, conversationData.nodes[convo[0].childrenIds[0]]]);
+    }
+  }, [convo]);
+
   return (
     <SafeAreaView flex="1">
       <StyledScrollView>
         <ChatWrap>
-          <ChatBubble type="received" text="Hello! Welcome to Lark!" />
+          {/* <ChatBubble type="received" text="Hello! Welcome to Lark!" />
           <ChatBubble type="received" text="Do you want to lose weight?" />
-          <ChatBubble type="sent" text="Yes" />
+          <ChatBubble type="sent" text="Yes" /> */}
+          {convo.map((msg, idx) => (
+            <ChatBubble type="received" text={msg.text} key={idx} />
+          ))}
         </ChatWrap>
       </StyledScrollView>
       <ButtonWrap>
